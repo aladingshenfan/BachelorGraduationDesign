3f698e56c5d356280c8ff8a05aa6fa677d26beab
Hao Yuefan <aladingshenfan@163.com>
2017-03-14 22:08:22 +0800

    2017-03-14

---
+++ Project/Hardware/ZRobot/src/IP_catalog/framebuffer_ip/framebuffer_ip/ip/frame_buffer/frame_buffer_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2014.2 (win64) Build 932637 Wed Jun 11 13:33:10 MDT 2014
+// Date        : Sun Jan 04 12:46:56 2015
+// Host        : xshxup15 running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/zhenyul/Xilinx_Proj_Jan/B3_BallTrack_BD/FrameBuffer/FrameBuffer.srcs/sources_1/ip/frame_buffer/frame_buffer_stub.v
+// Design      : frame_buffer
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_2,Vivado 2014.2" *)
+module frame_buffer(clka, wea, addra, dina, clkb, addrb, doutb)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[16:0],dina[15:0],clkb,addrb[16:0],doutb[15:0]" */;
+  input clka;
+  input [0:0]wea;
+  input [16:0]addra;
+  input [15:0]dina;
+  input clkb;
+  input [16:0]addrb;
+  output [15:0]doutb;
+endmodule
---
+++ Project/Hardware/ZRobot/src/IP_catalog/framebuffer_ip/framebuffer_ip/ip/frame_buffer/frame_buffer_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.2 (win64) Build 932637 Wed Jun 11 13:33:10 MDT 2014
+-- Date        : Sun Jan 04 12:46:56 2015
+-- Host        : xshxup15 running 64-bit Service Pack 1  (build 7601)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/zhenyul/Xilinx_Proj_Jan/B3_BallTrack_BD/FrameBuffer/FrameBuffer.srcs/sources_1/ip/frame_buffer/frame_buffer_stub.vhdl
+-- Design      : frame_buffer
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity frame_buffer is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 16 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    clkb : in STD_LOGIC;
+    addrb : in STD_LOGIC_VECTOR ( 16 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end frame_buffer;
+
+architecture stub of frame_buffer is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[0:0],addra[16:0],dina[15:0],clkb,addrb[16:0],doutb[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.2";
+begin
+end;
